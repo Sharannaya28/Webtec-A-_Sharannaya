a5c734fde11c39cc60041d18ea7cbb1078803c68
Sharannaya28 <101545256+Sharannaya28@users.noreply.github.com>
2023-05-13 00:17:20 +0600

    Delete Lab4 directory

--- Lab4/design/footer.php
+++
@@ -1,5 +0,0 @@
-        <tr>
-			<td colspan="2" style="background: green;">
-				<h3 align="center" style="background: green;color: white;padding: 20px 10px;">Copyright &copy; 2023. </h3>
-			</td>
-		</tr>
--- Lab4/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	session_start();
-	session_destroy();
-	setcookie("uname", "");
-	setcookie("pass", "");
-	setcookie("color", "");
-	header('location: home.php');
-?>
